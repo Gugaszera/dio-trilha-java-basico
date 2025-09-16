34d32d2ec19e9b06e64af93383202a76740b0760
Gugaszera <154289590+Gugaszera@users.noreply.github.com>
2025-09-15 22:25:52 -0300

    ContaTerminal.java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -4,18 +4,17 @@ public class ContaBanco {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita apenas agência e conta
         System.out.print("Por favor, digite o número da Agência: ");
         String agencia = scanner.next();
 
         System.out.print("Por favor, digite o número da Conta: ");
         int numero = scanner.nextInt();
 
-        // Dados fixos
+    
         String nomeCliente = "Mario Andrade";
         double saldo = 237.48;
 
-        // Monta a mensagem final
+       
         String mensagem = "Olá ".concat(nomeCliente)
             .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
             .concat(agencia)
